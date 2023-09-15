523533f04fa1f8efdfd8d601f8755bbf9d6c7e2e
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2023-09-15 11:34:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from similarity import preprocess_data, calculate_similarity
 import requests
 import subprocess
 import pandas as pd
-import pandas_profiling
 import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
