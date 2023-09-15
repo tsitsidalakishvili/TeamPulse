0c878df124225a04585760d5bcdd658cf5a05063
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2023-09-15 12:45:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,11 @@ from similarity import preprocess_data, calculate_similarity
 import requests
 import subprocess
 import pandas as pd
-import streamlit as st
+
 import pandas_profiling
 
+import streamlit_pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
 
 # Download stopwords if not already downloaded
