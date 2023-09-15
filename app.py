0d59d3bfc71bd2cd5c6fdcbdd0301e808a463d22
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2023-09-15 11:37:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import subprocess
 import pandas as pd
 import streamlit as st
 
-from streamlit_pandas_profiling import st_profile_report
 
 # Download stopwords if not already downloaded
 nltk.download('stopwords')
